7c2b3c727de03050ff59c75be3b6a1408c043919
Aryan <aryanmewada8518@gmail.com>
2024-03-10 17:53:36 +0530

    reac js

--- reduxtoolkitTodo/src/features/todo/todoSlice.js
+++ reduxtoolkitTodo/src/features/todo/todoSlice.js
@@ -5,6 +5,24 @@ const initialState = {
 }
 
 
-const todoSlice = createSlice({
+export const todoSlice = createSlice({
     name: 'todo',
+    initialState,
+    reducers:{
+        addTodo: (state, action) => {
+            const todo ={
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+            
+        },
+        removeTodo:(state, action) => {
+              state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+
+    }
 })
+export const {addTodo,removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
